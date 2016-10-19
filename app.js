c99b654665ef78ff0a3e802498b47d3419fa9bed
Frank <gcobs076761@gmail.com>
2016-10-19 16:25:24 +0800

    javascript

---
+++ app.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+$('#Myphoto').mouseover(function() {
+$('#Myphoto').attr('src','https://scontent-tpe1-1.xx.fbcdn.net/v/t1.0-9/13567260_1049376531822988_8630116064086618334_n.jpg?oh=4720d936b3f78c184174d1a881b61604&oe=586342C1');
+});
+
+$('#Myphoto').mouseout(function(){
+$('#Myphoto').attr('src','https://scontent-tpe1-1.xx.fbcdn.net/v/t1.0-9/11870679_868846139876029_3528942813270928697_n.jpg?oh=c66f8f6bc2e11a57d8eb2cd25d052c4d&oe=58A3F32B');
+});
+});
